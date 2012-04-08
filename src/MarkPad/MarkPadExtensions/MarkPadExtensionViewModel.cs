6abf11bc060871b1b849c85c370deb835d2a9eb6
Ben Scott <ben@belfryimages.com.au>
2012-04-08 20:29:59 +1000

    looks like install/uninstall works, not testable yet

--- src/MarkPad/MarkPadExtensions/MarkPadExtensionViewModel.cs
+++ src/MarkPad/MarkPadExtensions/MarkPadExtensionViewModel.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NuGet;
+using Caliburn.Micro;
 
 namespace MarkPad.MarkPadExtensions
 {
-	public class MarkPadExtensionViewModel
+	public class MarkPadExtensionViewModel : PropertyChangedBase
 	{
+		readonly IPackageManager _packageManager;
+
 		IPackage _package;
 
-		public MarkPadExtensionViewModel(IPackage package)
+		public MarkPadExtensionViewModel(
+			IPackageManager packageManager,
+			IPackage package)
 		{
+			_packageManager = packageManager;
+
 			_package = package;
 		}
 
@@ -31,6 +38,48 @@ namespace MarkPad.MarkPadExtensions
 				if (!string.IsNullOrEmpty(_package.Description)) return _package.Description;
 				return "";
 			}
+		}
+		bool IsInstalled
+		{
+			get
+			{
+				return _packageManager.LocalRepository.Exists(_package.Id);
+			}
+		}
+
+		public bool CanInstall { get { return !IsInstalled; } }
+		public void Install()
+		{
+			if (IsInstalled)
+			{
+				Uninstall();
+				return;
+			}
+
+			System.Windows.MessageBox.Show(this.Title);
+
+			_packageManager.InstallPackage(_package, false, false);
+			NotifyChangedInstallationStatus();
+		}
+
+		public bool CanUpdate { get { return false; } }
+		public void Update()
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool CanUninstall { get { return IsInstalled; } }
+		public void Uninstall()
+		{
+			_packageManager.UninstallPackage(_package, false, true);
+			NotifyChangedInstallationStatus();
+		}
+
+		void NotifyChangedInstallationStatus()
+		{
+			this.NotifyOfPropertyChange(() => CanInstall);
+			this.NotifyOfPropertyChange(() => CanUpdate);
+			this.NotifyOfPropertyChange(() => CanUninstall);
 		}
 	}
 }
